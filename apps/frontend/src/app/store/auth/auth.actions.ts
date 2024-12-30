6451c865e8612d6410c8a37cc5c119a4f0143f27
Dmitriy Chernykh <dchernykh@right-code.pro>
2024-12-30 14:08:00 +0400

    [feat] - auth actions

--- apps/frontend/src/app/store/auth/auth.actions.ts
+++ apps/frontend/src/app/store/auth/auth.actions.ts
@@ -1,5 +1,6 @@
 import {createAction, props} from '@ngrx/store';
 import {HttpErrorResponse} from '@angular/common/http';
+import { AuthAction } from '../store.enums';
 
 export const getEducationalProgramApprovedList = createAction(AuthAction.REGISTER);
 export const getEducationalProgramApprovedListSuccess = createAction(AuthAction.REGISTER_SUCCESS, props<{
--- apps/frontend/src/app/store/store.enums.ts
+++ apps/frontend/src/app/store/store.enums.ts
@@ -1,3 +1,9 @@
 export enum StoreKey {
     AUTH = 'auth',
 }
+
+export enum AuthAction {
+    REGISTER = '[Auth] Register',
+    REGISTER_SUCCESS = '[Auth] Register Success',
+    REGISTER_ERROR = '[Auth] Register Error'
+}
