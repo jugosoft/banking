5c90a98985994ade81750f117a530801963652d0
Jugosoft <sardimka@gmail.com>
2026-07-18 22:27:23 +0400

    feat(current-worth): added new field to banner

--- src/app/modules/shared/invest-card/invest-card.component.ts
+++ src/app/modules/shared/invest-card/invest-card.component.ts
@@ -9,39 +9,39 @@ import { filter } from 'rxjs';
 import { ConfirmModalData } from '../confirm-delete-modal';
 
 @Component({
-    selector: 'banking-invest-card',
-    standalone: true,
-    templateUrl: './invest-card.component.html',
-    styleUrl: './invest-card.component.scss',
-    imports: [CurrencyPipe, MaterialModule, CardLayoutComponent],
+  selector: 'banking-invest-card',
+  standalone: true,
+  templateUrl: './invest-card.component.html',
+  styleUrl: './invest-card.component.scss',
+  imports: [CurrencyPipe, MaterialModule, CardLayoutComponent],
 })
 export class InvestCardComponent {
-    @Input() public invest!: IInvestListItem;
-    @Output() public readonly delete = new EventEmitter<number>();
-    private readonly router = inject(Router);
-    private readonly modalService = inject(ModalService);
+  @Input() public invest!: IInvestListItem;
+  @Output() public readonly delete = new EventEmitter<number>();
+  private readonly router = inject(Router);
+  private readonly modalService = inject(ModalService);
 
-    public formatDate(date: Date): string {
-        const d = new Date(date);
-        return `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()}`;
-    }
+  public formatDate(date: Date): string {
+    const d = new Date(date);
+    return `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()}`;
+  }
 
-    public onDelete(): void {
-        const modalData: ConfirmModalData = {
-            title: 'Подтверждение удаления',
-            message: 'Вы уверены, что хотите удалить этУ поганую инвестицию?',
-            confirmText: 'Удалить',
-            cancelText: 'Отмена'
-        };
+  public onDelete(): void {
+    const modalData: ConfirmModalData = {
+      title: 'Подтверждение удаления',
+      message: 'Вы уверены, что хотите удалить этУ поганую инвестицию?',
+      confirmText: 'Удалить',
+      cancelText: 'Отмена'
+    };
 
-        this.modalService.openConfirmDeleteModal(modalData).pipe(
-            filter(result => !!result)
-        ).subscribe(() => {
-            this.delete.emit(this.invest.id);
-        });
-    }
+    this.modalService.openConfirmDeleteModal(modalData).pipe(
+      filter(result => !!result)
+    ).subscribe(() => {
+      this.delete.emit(this.invest.id);
+    });
+  }
 
-    public editInvest(): void {
-        void this.router.navigate(['/invest', 'edit', this.invest.id]);
-    }
+  public editInvest(): void {
+    void this.router.navigate(['/invest', 'edit', this.invest.id]);
+  }
 }
